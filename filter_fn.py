4ffe22c620b76f9eca190db31ede2bfac604140f
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-03 17:29:00 +0800

    cc

--- filter_fn.py
+++ filter_fn.py
@@ -5,10 +5,11 @@ from tqdm import tqdm
 
 if __name__ == "__main__":
     tar_dir = '/shared/xudongliu/bdd100k/100k/train/'
-    ref_dir = '/shared/xudongliu/bdd100k/labels/voc_val/class_labels/'
+    ref_dir = '/shared/xudongliu/bdd100k/labels/voc_train/class_labels/'
     fns = os.listdir(tar_dir)
     ref_fns = os.listdir(ref_dir)
     ref_name = [fn.split('.')[0] for fn in ref_fns]
+    print(ref_name[0])
     for fn in tqdm(fns):
         if fn.split('.')[0] not in ref_name:
             os.remove(os.path.join(tar_dir, fn))
