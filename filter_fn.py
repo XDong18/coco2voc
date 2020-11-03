a85137d57fe2207e99981005b6132f432664327f
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-03 17:35:52 +0800

    cc

--- filter_fn.py
+++ filter_fn.py
@@ -11,8 +11,10 @@ if __name__ == "__main__":
     ref_name = [fn.split('.')[0] for fn in ref_fns]
     tar_name = [fn.split('.')[0] for fn in fns]
     diff = [x for x in tar_name if x not in ref_name]
-    print(diff)
-    print(len(diff))
+    # print(diff)
+    # print(len(diff))
+    for fn in diff:
+        os.remove(os.path.join(tar_dir, fn + '.jpg'))
     # print(ref_name[0])
     # for fn in tqdm(fns):
     #     if fn.split('.')[0] not in ref_name:
