69e91a07b72f71ca45ad8e40812b236f22390a14
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-03 17:38:54 +0800

    cc

--- filter_fn.py
+++ filter_fn.py
@@ -4,15 +4,15 @@ from tqdm import tqdm
 
 
 if __name__ == "__main__":
-    tar_dir = '/shared/xudongliu/bdd100k/100k/train/'
-    ref_dir = '/shared/xudongliu/bdd100k/labels/voc_train/class_labels/'
+    tar_dir = '/shared/xudongliu/bdd100k/100k/val/'
+    ref_dir = '/shared/xudongliu/bdd100k/labels/voc_val/class_labels/'
     fns = os.listdir(tar_dir)
     ref_fns = os.listdir(ref_dir)
     ref_name = [fn.split('.')[0] for fn in ref_fns]
     tar_name = [fn.split('.')[0] for fn in fns]
     diff = [x for x in tar_name if x not in ref_name]
-    # print(diff)
-    # print(len(diff))
+    print(diff)
+    print(len(diff))
     for fn in diff:
         os.remove(os.path.join(tar_dir, fn + '.jpg'))
     # print(ref_name[0])
