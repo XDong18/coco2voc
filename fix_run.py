7a18539fedd26dbb8621efaabe2f49f4ad11a623
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-03 16:15:03 +0800

    cc

--- fix_run.py
+++ fix_run.py
@@ -4,8 +4,8 @@ from tqdm import tqdm
 
 
 if __name__ == "__main__":
-    tar_dir = '/shared/xudongliu/bdd100k/labels/voc_val/class_labels'
-    coco_fn = '/shared/xudongliu/bdd100k/labels/bdd100k_labels_images_det_coco_val.json'
+    tar_dir = '/shared/xudongliu/bdd100k/labels/voc_train/class_labels'
+    coco_fn = '/shared/xudongliu/bdd100k/labels/bdd100k_labels_images_det_coco_train.json'
     coco = COCO(coco_fn)
     imgs = coco.imgs
     for img in tqdm(imgs):
