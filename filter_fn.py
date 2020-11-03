48a6e89d0591241bbae81ad139eaeae068d0bfff
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-03 17:23:59 +0800

    cc

---
+++ filter_fn.py
@@ -0,0 +1,16 @@
+import os
+# from pycocotools.coco import COCO
+from tqdm import tqdm
+
+
+if __name__ == "__main__":
+    tar_dir = '/shared/xudongliu/bdd100k/100k/train/'
+    ref_dir = '/shared/xudongliu/bdd100k/labels/voc_val/class_labels/'
+    fns = os.listdir(tar_dir)
+    ref_fns = os.listdir(ref_dir)
+    ref_name = [fn.split('.')[0] for fn in ref_fns]
+    for fn in tqdm(fns):
+        if fn.split('.') not in ref_name:
+            os.remove(os.path.join(tar_dir, fn))
+            print(fn)
+            break
